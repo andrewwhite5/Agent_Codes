976b5cbac5192deca4261823b706b00994092c61
White <Andrew_White@moduslink.com>
2020-02-12 15:25:31 -0700

    Added AHT and Avg Working Rate

--- Graphs.py
+++ Graphs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
+from decimal import Decimal
 
 '''
 Import inContact spreadsheets
@@ -111,9 +112,6 @@ break_df['Agent Name'] = break_df['Agent Name'].str.replace(
     '4', '').str.replace('5', '').str.replace('6', '').str.replace(
     '7', '').str.replace('8', '').str.replace('9', '')
 
-# Sort by highest Case Work duration
-break_df = break_df.sort_values('Duration', ascending=False)
-
 # Define Break Compliance (108% of total break time)
 break_time = (.5/8)
 break_compliance_basic = 1.08
@@ -140,10 +138,10 @@ over_comp = break_df.where(break_df['Percent'] > 1.08)
 over_comp = over_comp.dropna()
 over_comp['Percent'] = over_comp['Percent']*100
 over_comp = over_comp.sort_values('Percent', ascending=False)
-print('Number of agents who are over break compliance:', len(over_comp))
-over_comp.head(len(over_comp))
+print('\nNumber of agents who are over break compliance:', len(over_comp))
+print(over_comp)
 
-# Sort values
+# Sort values differently for graph
 over_comp = over_comp.sort_values('Percent', ascending=True)
 
 # Choose plot style
@@ -177,8 +175,6 @@ ax.spines['right'].set_color('none')
 ax.spines['left'].set_smart_bounds(True)
 ax.spines['bottom'].set_smart_bounds(True)
 plt.show()
-
-print(over_comp)
 
 
 '''
@@ -223,9 +219,6 @@ case_work['Agent Name'] = case_work['Agent Name'].str.replace(
     '4', '').str.replace('5', '').str.replace('6', '').str.replace(
     '7', '').str.replace('8', '').str.replace('9', '')
 
-# Sort by highest Case Work duration
-case_work = case_work.sort_values('Duration', ascending=False)
-
 # Change percent to whole number
 case_work['Percent'] = case_work['Percent'] * 100
 
@@ -242,17 +235,15 @@ case_work = case_work.drop(columns='Team Name (ID)')
 '''
 Visualize Case Work
 '''
-
-# Create dataframe of agents who are spending more than 10% of their time in
-# Case Work
+# Create dataframe of agents who spent over 10% of their time in Case Work
 high_cw = case_work.where(case_work['Percent'] >= 10)
 high_cw = high_cw.dropna()
 high_cw = high_cw.sort_values('Percent', ascending=False)
-print('Number of agents who spend more than 10% of their time in Case Work:',
+print('\nNumber of agents who spend more than 10% of their time in Case Work:',
       len(high_cw))
-high_cw.head(len(high_cw))
+print(high_cw)
 
-# Sort values
+# Sort values differently for graph
 high_cw = high_cw.sort_values('Percent', ascending=True)
 
 # Choose plot style
@@ -287,4 +278,38 @@ ax.spines['left'].set_smart_bounds(True)
 ax.spines['bottom'].set_smart_bounds(True)
 plt.show()
 
-print(high_cw)
+
+'''
+Average Handle Time
+'''
+# Change values in time columns from str to timedelta
+snapshot['Inbound AHT'] = pd.to_timedelta(snapshot['Inbound AHT'])
+snapshot['Outbound AHT'] = pd.to_timedelta(snapshot['Outbound AHT'])
+snapshot['Available Time'] = pd.to_timedelta(snapshot['Available Time'])
+snapshot['Unavailable Time'] = pd.to_timedelta(snapshot['Unavailable Time'])
+snapshot['Login Time'] = pd.to_timedelta(snapshot['Login Time'])
+
+# Create new dataframe of agents with concerning Average Handle Time
+high_AHT = snapshot[snapshot['Inbound AHT'] > pd.Timedelta('13 min')]
+low_AHT = snapshot[snapshot['Inbound AHT'] < pd.Timedelta('5 min')]
+
+# Put the two together and remove zeros
+concerning_AHT = pd.concat([high_AHT, low_AHT])
+concerning_AHT = concerning_AHT[
+    concerning_AHT['Inbound AHT'] != pd.Timedelta('0 min')]
+
+#
+concerning_AHT = concerning_AHT.sort_values('Inbound AHT', ascending=True)
+print('\nAgents with potentially concerning average handle times:',
+    len(concerning_AHT))
+print(concerning_AHT)
+
+
+'''
+Average Working Rate
+'''
+# Convert Working Rate to float and calcualate mean (the goal is at least 80%)
+summary['Working Rate'] = summary['Working Rate'].str.strip('%').astype(float)
+raw_mean = Decimal(summary['Working Rate'].mean())
+avg_work_rt = round(raw_mean,2)
+print(f'\nAverage Working Rate: {avg_work_rt}%')
